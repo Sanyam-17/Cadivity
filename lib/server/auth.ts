a9598c914f338871914d0ec74270f443ca2cf32d
Sanyam-17 <139548715+Sanyam-17@users.noreply.github.com>
2026-05-29 17:28:54 +0530

    Update baseURL for betterAuth configuration

--- lib/server/auth.ts
+++ lib/server/auth.ts
@@ -6,7 +6,7 @@ import { emailOTP } from "better-auth/plugins";
 import { resend } from "./resend";
 
 export const auth = betterAuth({
- baseURL: process.env.BETTER_AUTH_URL || "https://cadivity.vercel.app",
+ baseURL: process.env.BETTER_AUTH_URL || "https://cadivity-idrz.vercel.app/",
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
